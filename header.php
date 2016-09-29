66393bda998f2019a8cadce7286191586e813a8e
hsiegm67 <horst.siegmann@yahoo.de>
2016-09-29 21:48:54 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,6 @@
+<?php echo "<h1> 31737 Rinteln</h1>"; ?>
+<?php include "sonne.php"; ?>
+<?php include "mond.php"; ?>
+<?php include "wetter.php"; ?>
+<?php include "windseite.php"; ?>
+<?php include "regenseite.php"; ?>
