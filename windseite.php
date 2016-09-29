33a18414b3f5a5a64ec671f4cd234e527d7076b6
hsiegm67 <horst.siegmann@yahoo.de>
2016-09-29 22:01:25 +0200

    Create windseite.php

---
+++ windseite.php
@@ -0,0 +1,16 @@
+<?php include "config.ini.php"; ?>
+<?php include "windmodul.php"; ?>
+<?php include "windrichtung.php"; ?>
+<img id="bilder" src="W.JPG" >
+<?php
+echo " Wind :  " .$wind ." km/h";
+echo " b&ouml;en :  " .$wind_boen ." km/h <br />";
+echo " Richtung :" .$richtung ." ";
+echo ": " .$wind_angle ." <br />";
+?>
+<a href="windseite2.php">weitere daten</a>
+<?php
+echo "<br> \n";
+echo "---------------------------------------------<br> \n";
+echo "<br> \n";
+?>
