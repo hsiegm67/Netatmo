a549f90b1bb57cff65455a4dfa073cdd634c4b5d
hsiegm67 <horst.siegmann@yahoo.de>
2016-09-29 22:00:41 +0200

    Update windseite2.php

--- windseite2.php
+++ windseite2.php
@@ -1,16 +1,15 @@
+
 <?php include "config.ini.php"; ?>
 <?php include "windmodul.php"; ?>
 <?php include "windrichtung.php"; ?>
-<img id="bilder" src="W.JPG" >
 <?php
-echo " Wind :  " .$wind ." km/h";
-echo " b&ouml;en :  " .$wind_boen ." km/h <br />";
-echo " Richtung :" .$richtung ." ";
-echo ": " .$wind_angle ." <br />";
-?>
-<a href="windseite2.php">weitere daten</a>
-<?php
-echo "<br> \n";
+// Windmodul
+echo " Wind :  " .$wind ." km/h <br> \n";
+echo " Windböen :  " .$wind_boen ." km/h <br> \n";
+echo " Windeichtung:  " .$wind_angle ."  <br> \n";
+echo " Maxi Windeichtung:  " .$wind_angle_max ." <br> \n";
+echo "Maxi male Windgeschweindigkeit:  " .$wind_max ."  km/h<br> \n";
+echo "Maxi male Windgeschweindigkeit um :  " .$wind_date_max ."  km/h<br> \n";
 echo "---------------------------------------------<br> \n";
-echo "<br> \n";
+
 ?>
