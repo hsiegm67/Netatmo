3123db9f39dd860538faba0b5f9c8da4bed06050
hsiegm67 <horst.siegmann@yahoo.de>
2018-07-16 16:39:48 +0200

    Delete header.php

--- header.php
+++
@@ -1,6 +0,0 @@
-<?php echo "<h1> 31737 Rinteln</h1>"; ?>
-<?php include "sonne.php"; ?>
-<?php include "mond.php"; ?>
-<?php include "wetter.php"; ?>
-<?php include "windseite.php"; ?>
-<?php include "regenseite.php"; ?>
